09003607ce1a5dd77c675a303e2e09696dd38e9f
U-Chive-PC\Chive <chivequiachon@gmail.com>
2019-02-28 00:29:24 +0800

    Added basic views and urls

--- Reviews/urls.py
+++ Reviews/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     # ex: /
     url(r'^$', views.song_review_list, name='song_review_list'),
+    # ex: /review/5/
+    url(r'^review/(?P<review_id>[0-9]+)/$', views.song_review_detail, name='song_review_detail'),
+    # ex: /song/
+    url(r'^song$', views.nogi_song_list, name='nogi_song_list'),
+    # ex: /song/5/
+    url(r'^song/(?P<nogi_song_id>[0-9]+)/$', views.nogi_song_detail, name='nogi_song_detail'),
+    # ex: /review/user - get reviews for the logged user
+    url(r'^review/user/(?P<username>\w+)/$', views.user_review_list, name='user_review_list'),
+    url(r'^review/user/$', views.user_review_list, name='user_review_list'),
 ]
--- Reviews/views.py
+++ Reviews/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from registration.backends.simple.views import RegistrationView    
-
+from .models import NogizakaSong, SongReview
 
 def song_review_list(request):
-    return render(request, 'song_review_list.html')
+    latest_reviews = SongReview.objects.order_by('-publish_date')[:9]
+    context = {'latest_reviews': latest_reviews}
+    return render(request, 'song_review_list.html', context)
+
+def song_review_detail(request, song_review_id):
+    song_review = get_object_or_404(SongReview, pk=song_review_id)
+    return render(request, 'song_review_detail.html', {'song_review': song_review})
+
+def nogi_song_list(request):
+    nogi_songs = NogizakaSong.objects.order_by('-name')
+    context = {'nogi_songs': nogi_songs}
+    return render(request, 'song_review_list.html', context)
+
+def nogi_song_detail(request, nogi_song_id):
+    nogi_song_detail = get_object_or_404(NogizakaSong, pk=nogi_song_id)
+    return render(request, 'song_review_list.html', {'nogi_song': nogi_song_detail})
+
+def user_review_list(request, username=None):
+    if not username:
+        username = request.user.username
+
+    latest_reviews = SongReview.objects.filter(user_name=username).order_by('-publish_date')
+    context = {'latest_reviews': latest_reviews, 'username': username}
+
+    ## TO BE CHANGED
+    return render(request, 'user_review_list.html', context)
